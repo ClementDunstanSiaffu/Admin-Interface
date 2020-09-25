c85824b9a7e4b0d40a08e467f472a985599d7d71
unknown <clementsiaffu95@gmail.com>
2020-09-25 16:04:33 +0300

    sixth commit

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import Typical from 'react-typical';
 /*import image1 from '../assets/images/libary.png';
 import image2 from '../assets/images/resume.png';
 import image3 from '../assets/images/graduu.jpg';*/
-/*const image1 = require('../assets/images/libary.png')
-const image2 = require('../assets/images/resume.png')*/
+/*const image1 = require('../assets/images/libary.png')*/
+const image2 = require('../assets/images/resume.png')
 const image3 = require('../assets/images/graduu.jpg')
 
 
@@ -73,7 +73,7 @@ function Home (){
                      
                     
                        <a href = "http://localhost:3000/resume" style = {{textDecoration:'none'}} >
-                        {/*<img src = {image2} className = "image-style"/>*/}
+                        <img src = {image2} className = "image-style"/>
                        <h1 className = "text-form">Resume form</h1>
                        </a>
                    
